640e421d6c6b2f2b801875780d5a587717cd4002
Katie Hoffman <ktahoffman@gmail.com>
2015-05-27 15:37:02 -0400

    rename custom JS files

--- public/javascripts/01-prevent-text-highlighting.js
+++
@@ -1,8 +0,0 @@
-(function($){
-    $.fn.disableSelection = function() {
-        return this
-                 .attr('unselectable', 'on')
-                 .css('user-select', 'none')
-                 .on('selectstart', false);
-    };
-})(jQuery);
